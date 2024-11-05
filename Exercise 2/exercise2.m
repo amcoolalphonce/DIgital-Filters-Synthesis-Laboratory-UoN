fbee5ed987c8ef2a5b813c8a88f41481e80d0beb
aposmasaa <amcoolalphonce@gmail.com>
2024-11-05 20:10:50 +0300

    Plot the frequency response

--- Exercise 2/exercise2.m
+++ Exercise 2/exercise2.m
@@ -14,3 +14,17 @@ sampling_frequency = 3*stopband_frequency; % twice the highest frequency using N
 % calculate cutoff freq to meet the passband specs
 passband_cutoff_frequency = passband_spec_w_c(passband_frequency_radians,passband_attenuationdB, filter_order)
 
+
+% PART B: PLOT THE FREQUENCY
+% get the transfer function of the butterworth filter
+[hs_num, hs_denom] = butter(filter_order, w_c, 'low', 's');
+hs = tf(hs_num, hs_denom) % analog filter transfer function
+% plot tthe frequency response of the analog filter
+options = bodeoptions;
+options.FreqUnits = 'Hz';
+options.FreqScale = 'linear';
+options.Xlim = [0, (sampling_frequency/2)];
+options.Grid = 'on';
+figure;
+bode(hs, options);
+title('Frequency Response of Analog Butterworth Filter');
