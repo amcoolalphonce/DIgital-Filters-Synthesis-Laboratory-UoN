6fa4119eaaae8047bc4e075058c8e1758ae1e7da
aposmasaa <amcoolalphonce@gmail.com>
2024-11-05 20:08:20 +0300

    Define passband and stopband parameters and get filter order

--- Exercise 2/exercise2.m
+++ Exercise 2/exercise2.m
@@ -1,1 +1,16 @@
+% PART A : Order of a Buuterworth Filter 
+
+passband_frequency = 5000;
+passband_frequency_radians = 2*pi*passband_frequency 
+passband_attenuationdB = 3; 
+
+stopband_frequency = 15000; % in Hz
+stopband_frequency_radians = 2*pi*stopband_frequency % in rad/s
+stopband_attenuationdB = 40; % stopband attenuation in dB
+sampling_frequency = 3*stopband_frequency; % twice the highest frequency using Nyquist theorem
+
+
+[filter_order, cut_off_frequency] = buttord(passband_frequency_radians, stopband_frequency_radians, passband_attenuationdB, stopband_attenuationdB,'s')
+% calculate cutoff freq to meet the passband specs
+passband_cutoff_frequency = passband_spec_w_c(passband_frequency_radians,passband_attenuationdB, filter_order)
 
